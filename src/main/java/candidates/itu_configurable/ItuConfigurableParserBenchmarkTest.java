63fd5ce2db7abe00fdc114090406724136c6b40f
Morten Haraldsen <dev@ethlo.com>
2024-02-06 07:33:20 +0100

    Update test

--- src/main/java/candidates/itu_configurable/ItuConfigurableParserBenchmarkTest.java
+++ src/main/java/candidates/itu_configurable/ItuConfigurableParserBenchmarkTest.java
@@ -1,51 +1,40 @@
 package candidates.itu_configurable;
 
 
+import static com.ethlo.time.DateTimeTokens.digits;
+import static com.ethlo.time.DateTimeTokens.fractions;
+import static com.ethlo.time.DateTimeTokens.separators;
+import static com.ethlo.time.DateTimeTokens.zoneOffset;
 import static com.ethlo.time.Field.DAY;
 import static com.ethlo.time.Field.HOUR;
 import static com.ethlo.time.Field.MINUTE;
 import static com.ethlo.time.Field.MONTH;
 import static com.ethlo.time.Field.SECOND;
 import static com.ethlo.time.Field.YEAR;
-import static com.ethlo.time.token.DateTimeTokens.digits;
-import static com.ethlo.time.token.DateTimeTokens.fractions;
-import static com.ethlo.time.token.DateTimeTokens.separators;
-import static com.ethlo.time.token.DateTimeTokens.timeZoneOffset;
 
 import java.text.ParsePosition;
 
+import com.ethlo.time.DateTimeParser;
 import com.ethlo.time.token.ConfigurableDateTimeParser;
 import common.ParserBenchmarkTest;
 
 public class ItuConfigurableParserBenchmarkTest extends ParserBenchmarkTest
 {
-    private static final ConfigurableDateTimeParser DATE = new ConfigurableDateTimeParser(
+    private static final DateTimeParser FRACTIONAL_SECONDS_OFFSET = ConfigurableDateTimeParser.of(
             digits(YEAR, 4),
             separators('-'),
             digits(MONTH, 2),
             separators('-'),
-            digits(DAY, 2)
-    );
-
-    private static final ConfigurableDateTimeParser MINUTES = DATE.combine(
+            digits(DAY, 2),
             separators('T'),
             digits(HOUR, 2),
             separators(':'),
-            digits(MINUTE, 2)
-    );
-
-    private static final ConfigurableDateTimeParser LOCAL_TIME = MINUTES.combine(
+            digits(MINUTE, 2),
             separators(':'),
-            digits(SECOND, 2)
-    );
-
-    private static final ConfigurableDateTimeParser FRACTIONAL_SECONDS_LOCAL = LOCAL_TIME.combine(
+            digits(SECOND, 2),
             separators('.'),
-            fractions()
-    );
-
-    private static final ConfigurableDateTimeParser FRACTIONAL_SECONDS_OFFSET = FRACTIONAL_SECONDS_LOCAL.combine(
-            timeZoneOffset()
+            fractions(),
+            zoneOffset()
     );
 
     public ItuConfigurableParserBenchmarkTest()
